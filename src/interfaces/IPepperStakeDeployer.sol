181f41b0adff999395c9829f6dda9f1de5e3fa92
Jonathan Miller <jon@jmill.dev>
2022-11-11 19:17:24 -0500

    Allowlist working

--- src/interfaces/IPepperStakeDeployer.sol
+++ src/interfaces/IPepperStakeDeployer.sol
@@ -2,29 +2,22 @@
 pragma solidity ^0.8.13;
 
 import "./IPepperStake.sol";
+import "../structs/LaunchPepperStakeData.sol";
 
 interface IPepperStakeDeployer {
-    event DeployPepperStake(
-        IPepperStake indexed pepperStake,
-        address[] _supervisors,
-        uint256 _stakeAmount,
-        address[] _unreturnedStakeBeneficiaries,
-        uint256 _returnWindowDays,
-        uint256 _maxParticipants,
-        bool _shouldParticipantsShareUnreturnedStake,
-        bool _shouldUseSupervisorInactionGuard,
-        string _metadataURI
-    );
+    // event DeployPepperStake(
+    //     IPepperStake indexed pepperStake,
+    //     address[] _supervisors,
+    //     uint256 _stakeAmount,
+    //     address[] _unreturnedStakeBeneficiaries,
+    //     uint256 _returnWindowDays,
+    //     uint256 _maxParticipants,
+    //     bool _shouldParticipantsShareUnreturnedStake,
+    //     bool _shouldUseSupervisorInactionGuard,
+    //     string _metadataURI
+    // );
 
-    function deployPepperStake(
-        address[] memory _supervisors,
-        uint256 _stakeAmount,
-        address[] memory _unreturnedStakeBeneficiaries,
-        uint256 _returnWindowDays,
-        uint256 _maxParticipants,
-        bool _shouldParticipantsShareUnreturnedStake,
-        bool _shouldUseSupervisorInactionGuard,
-        string memory _metadataURI,
-        address[] memory _oracleDelegates
-    ) external returns (IPepperStake pepperStake);
+    function deployPepperStake(LaunchPepperStakeData memory _launchData)
+        external
+        returns (IPepperStake pepperStake);
 }
---
+++ src/structs/ParticipantData.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+struct ParticipantData {
+    bool isAllowedToParticipate;
+    bool participated;
+    bool completed;
+}
